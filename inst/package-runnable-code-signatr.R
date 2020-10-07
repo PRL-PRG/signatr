2dc694f6183539988b9b7c3ae0dfc80afeba7d52
Filip Krikava <krikava@gmail.com>
2020-10-07 13:07:20 +0000

    Add package-runnable-code-signatr.R

---
+++ inst/package-runnable-code-signatr.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+
+options(error = function() { traceback(3); q(status=1) })
+
+library(glue)
+library(runr)
+
+wrap <- function(package, file, type, body) {
+  body <- paste("      ", body, collapse="\n")
+  glue(
+    "evil::write_eval_traces(",
+    "  evil::trace_eval(",
+    "    quote = TRUE,",
+    "    code = {{",
+    "{body}",
+    "    }}",
+    "  ),",
+    "  datadir=file.path(Sys.getenv('RUNR_CWD'), basename('{file}'))",
+    ")",
+    .sep = "\n"
+  )
+}
+
+OUTPUT_FILE <- "runnable-code.csv"
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 1) {
+  stop("Missing a path to the package source")
+}
+
+package_path <- args[1]
+package <- basename(package_path)
+
+files <- runr::extract_package_code(
+  package,
+  package_path,
+  types="all",
+  output_dir=".",
+  compute_sloc=TRUE,
+  wrap_fun=wrap
+)
+
+write.csv(df, OUTPUT_FILE, row.names=FALSE)
