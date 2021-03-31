1790589d12d73ba0066e001bca8849df9d761577
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-03-31 23:18:52 +0200

    new signatr experiment

--- inst/run-signatr.R
+++ inst/run-signatr.R
@@ -5,14 +5,17 @@ library(record)
 
 args <- commandArgs(TRUE)
 
-if (length(arguments) < 1) {
-  message("USAGE: ./run-one-new.R [csv_path] [db_path] [num_fun] [num_run]")
+if (length(args) < 1) {
+  message("USAGE: ./run-signatr.R [functions_path] [db_path] [num_fun] [num_run]")
   q(status=1)
 }
 
-functions_df <- read.csv(args[1])
-db <- open_db(args[2], create = FALSE)
+functions_df <- read.csv(arg[1])
 
+## package_name <- paste0("/mnt/nvme1/R/project-signatR/run/package-metadata/", args[1])
+## package_df <- functions_df[functions_df$package == package_name, ]
+
+db <- open_db(args[2], create = FALSE)
 if(!is.null(db)) {
   stop("could not open db")
 }
@@ -68,61 +71,4 @@ for(i in seq_along(fun_list)) {
 }
 
 
-write.csv(run_results, "signatr_results.csv")
-
-
-
-## run_until_killed(function() { # function to be run
-##   # Deal with arguments used for function call
-##   fargs = formals(func)
-##   fargs_names = names(fargs)
-
-##   tryCatch(
-##   {
-##     if (length(fargs) == 0) {
-##         src_hash = paste(package, arguments[2], "NO_PARAMS", sep="::")
-##         calls_record[nrow(calls_record) + 1, ] <<- c(call_id, src_hash, "NO_VALUE")
-##     } else {
-##       for (name in fargs_names) {
-##     	src_hash = paste(package, arguments[2], name, sep="::")
-##     	hash = get_random_hash(GBOV)
-##         value = look_up(GBOV, hash)
-##         # val_hash = digest::sha1(value) # Need to make sure this is consistent
-##         calls_record[nrow(calls_record) + 1, ] <<- c(call_id, src_hash, hash)
-##         fargs[name] = value
-##       }
-##     }
-
-##     print(paste("*** Call ID:", call_id, "***\n", sep=" "))
-##     ret = do.call(func, as.list(fargs))
-##     print(typeof(ret))
-
-##     # Add the result value to GBOV if it's not in there already
-##     # so that it can be used in future tests
-##     # gbov <<- add_value(GBOV, ret)
-
-##     results_record[nrow(calls_record) + 1, ] <<- c(call_id, typeof(ret), "stdout", "stderr")
-##     successes <<- successes + 1
-##   }, warning = function(warn) {
-##     print("WARN")
-##     # results_record[nrow(calls_record) + 1, ] <<- c(call_id, as.character(warn), "stdout", "stderr")
-##   },error = function(err) {
-##     print("ERR")
-##     print(as.character(err))
-##     # results_record[nrow(calls_record) + 1, ] <<- c(call_id, as.character(err), "stdout", "stderr")
-##   })
-
-##   call_id <<- call_id + 1
-##   if (call_id == 301) {
-##     stop()
-##   }
-## }, function() { # function run at death
-  ## if (call_id < 1001) { # encountered joker function
-  ##   jokers <- readrds(arguments[4])
-  ##   jokers <- unique(append(jokers, paste(package, arguments[2], sep = "::")))
-  ##   saverds(jokers, file = arguments[4])
-  ## }
-  # dir.create(paste(package, arguments[2], sep = "/"), recursive = TRUE)
-##   write.csv(calls_record, paste(package, arguments[2], "calls.csv", sep = "::"), row.names = F)
-##   write.csv(results_record, paste(package, arguments[2], "results.csv", sep = "::"), row.names = F)
-## })
+write.csv(run_results, "signatr_results.csv", row.names=FALSE)
