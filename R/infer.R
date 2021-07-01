2c05ce747087e4f384f2aaf156b1614f686ca687
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-07-01 14:32:56 +0200

    update

--- R/infer.R
+++ R/infer.R
@@ -1,66 +1,81 @@
 #' @export
-# infer type signature of a given function
+# run the given function with values from value generator
 # @param package a string representation of package name
 # @param fun  a string representation of function name
 # @param params number of formals 
-# @param db path to database to sample values from
+# @param vg value generator 
 # @param runs number of runs to try plugging sampled vals
 # @return a list of type signature of fun if successful
 #' @importFrom record open_db close_db sample_val size_db
 
-infer_signature <- function(package = NULL, fun, params, db, runs) {
-  if(dir.exists(db)) {
-    db <- open_db(db, create = FALSE)
-  } else {
-    stop("db doesn't exist.")
-  }
+run_fun <- function(package = NULL, fun, params, vg = c("random_int", "random_char", "random_double", "db"), db = NULL, runs) {
 
-  run_results <<- data.frame(package = character(0),
-                             fun = character(0),
-                             input = character(0),
-                             output = character(0),
-                             exitval = integer(0),
-                             errmsg = character(0),
-                             stringsAsFactors = FALSE)
+  result <- data.frame(package = character(0),
+                       fun = character(0),
+                       input = character(0),
+                       output = character(0),
+                       exitval = integer(0),
+                       msg = character(0),
+                       stringsAsFactors = FALSE)
 
   clo <- get(fun, envir=getNamespace(package), mode="function")
 
-  for (j in 1:runs) {
-    stopifnot(size_db() >= params)
+  run_results <- lapply(seq(runs), function(r) {
+    switch (vg,
+            "random_int" = {
+              args <- sample.int(100, params, replace = FALSE)},
+            "random_double" = {
+              args <- runif(params, 0, 100)},
+            "random_char" = {
+              args <- lapply(runif(params, 0, 100), as.character)},
+            {
+              if(dir.exists(db)) {
+                db <- open_db(db, create = FALSE)
+                stopifnot(size_db() >= params)
 
-    id <- sample.int(size_db(), params)
-    arg_list <- lapply(id, function(id) sample_val())
+                id <- sample.int(size_db(), params)
+                args <- lapply(id, function(id) sample_val())
+              } else {
+                stop("db doesn't exist.")
+              }
+            })
 
-    input_type <- toString(lapply(arg_list, typeof))
+    ## input_type <- toString(lapply(args, typeof))
+    res <- data.frame(package = package, fun = fun, input = paste(args, collapse=','), output = NA, exitval = NA, errmsg = NA, stringsAsFactors = FALSE)
 
     tryCatch ({
-      res <- do.call(clo, arg_list)
-      output_type <- toString(typeof(res))
+      output <- do.call(clo, as.list(args))
+      ## output_type <- toString(typeof(res))
 
-      obs <- data.frame(package = package, fun = fun, input = input_type, output = output_type, exitval = 0L, errmsg = NA)
-      run_results <<- rbind(obs, run_results)
+      res$output <- output
+      res$exitval <- 0L
+      res
+    }, warning = function(w) {
+      res$exitval <- 1L
+      res$errmsg <- as.character(w)
+      res
+      ## data.frame(package = package, fun = fun, intput = args, output = NA, exitval = 1L, msg = as.character(w))
     }, error = function(e) {
-      obs <- data.frame(package = package, fun = fun, intput = input_type, output = NA, exitval = 1L, errmsg = as.character(e))
-      run_results <<- rbind(obs, run_results)
+      res$exitval <- 2L
+      res$errmsg <- as.character(e)
+      res
+      ## data.frame(package = package, fun = fun, intput = args, output = NA, exitval = 2L, errmsg = as.character(e))
     })
-  }
+  })
 
-  print(run_results)
-  
-  is_successful <- 0L %in% run_results$exitval
+  result <- do.call(rbind, run_results)
 
-  if(is_successful) {
-    successful_runs <- which(run_results$exitval == 0L)
-    res <- lapply(successful_runs, function(id) c(run_results[id,]$input, fun_results[id,]$output))
-  } else {
-    res <- character(0)
-  }
-  res
+  result
 }
 
+infer_sig <- function(res) {
+  not_errored <- 0L %in% res$exitval
 
-
-
-
-
-
+  if(not_errored) {
+    runs <- which(res$exitval == 0L)
+    types <- lapply(runs, function(r) data.frame(intput = res[r,]$input, output = res[r,]$output))
+  } else {
+    types <- character(0)
+  }
+  types
+}
