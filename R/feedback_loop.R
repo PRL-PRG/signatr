eea46d0b553371a61a624e830697d7c3669a8d84
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-07-19 15:57:28 +0200

    back to run_fun

--- R/feedback_loop.R
+++ R/feedback_loop.R
@@ -58,7 +58,7 @@ feedback_loop <- function (package = NA,
       }
 
       args <- params
-      new_state <- run_func(package, fun_name, fun, args)
+      new_state <- run_fun(package, fun_name, fun, args)
       state <- rbind(state, new_state)
       budget <- budget - 1
     }
@@ -85,7 +85,7 @@ feedback <- function(history, tolerance, state) {
 #' @param args       arguments to run the function with
 #' @return           list of metadata and running result
 #' @export
-run_func <- function(package, fun_name, fun, args) {
+run_fun <- function(package, fun_name, fun, args) {
   res <- tryCatch ({
     output <- do.call(fun, as.list(args))
     list(package, fun_name, length(args), args, output, 0L, NA, NA)
