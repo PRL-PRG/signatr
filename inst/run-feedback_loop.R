acc2252db809040ed676efb66c6d5a098789cbb1
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-07-22 16:09:30 +0200

    improve print message and metadata format

--- inst/run-feedback_loop.R
+++ inst/run-feedback_loop.R
@@ -14,6 +14,8 @@ package <- args[[1]]   # stringr | reshape2
 budget <- as.double(args[[2]])    # 343
 tolerance <- as.double(args[[3]])  # 1
 
+print(paste0("Package: ", package))
+
 tic("generating data")
 ns <- getNamespace(package)
 
@@ -31,6 +33,17 @@ fun_df <- data.frame(
 
 filtered <- fun_df[fun_df$num_param < 10, ]
 
+param1 <- nrow(fun_df[fun_df$num_param == 1,])
+param2 <- nrow(fun_df[fun_df$num_param == 2,])
+param3 <- nrow(fun_df[fun_df$num_param == 3,])
+
+
+print(paste0("Number of functions: ", length(exported_functions)))
+print(paste0("Number of one argument functions: ", param1))
+print(paste0("Number of two argument functions: ", param2))
+print(paste0("Number of three argument functions: ", param3))
+
+
 state <- lapply(filtered$fun, function(fun) feedback_loop(package = package,
                                                                 fun_name = fun,
                                                                 budget = budget,
@@ -43,37 +56,33 @@ saveRDS(data, file = paste0(package, "_data.RDS"))
 generating <- toc()
 
 tic("data analysis")
-print(paste0("number of functions in ", package, ": ", length(exported_functions)))
-print(paste0("total of function calls made: ", nrow(data)))
+
+print(paste0("Total of function calls made: ", nrow(data)))
 
 success <- data[data[,6] == 0L,]
 success_rate <- nrow(success) / nrow(data) * 100                # 21.9% | 30.9%
 
-print(paste0("success rate for ", package, ": ", success_rate))
+print(paste0("Over all success rate: ", success_rate))
 
-param1 <- data[data[,3] == 1,]                   #nrow(param1) = 21
-param2 <- data[data[,3] == 2,]                   #nrow(param2) = 980
-param3 <- data[data[,3] == 3,]                   #nrow(param3) = 5145
-                                                 #              ------
+calls_param1 <- data[data[,3] == 1,]                   #nrow(param1) = 21
+calls_param2 <- data[data[,3] == 2,]                   #nrow(param2) = 980
+calls_param3 <- data[data[,3] == 3,]                   #nrow(param3) = 5145
 
-param123 <- do.call(rbind, list(param1, param2, param3))       # 6146
-param123_success <- param123[param123[,6] == 0L,]              # 1526
-param123_success_rate <- nrow(param123_success) / nrow(param123) * 100       # 24.8% | 30.9%
-
+calls_param123 <- do.call(rbind, list(calls_param1, calls_param2, calls_param3))       # 6146
+calls_param123_success <- calls_param123[calls_param123[,6] == 0L,]              # 1526
+calls_param123_success_rate <- nrow(calls_param123_success) / nrow(calls_param123) * 100       # 24.8% | 30.9%
 
 
-print(paste0("number of calls to one argument functions: ", nrow(param1)))
-print(paste0("number of calls to two argument functions: ", nrow(param2)))
-print(paste0("number of calls to three argument functions: ", nrow(param3)))
+print(paste0("Number of calls to one argument functions: ", nrow(calls_param1)))
+print(paste0("Number of calls to two argument functions: ", nrow(calls_param2)))
+print(paste0("Number of calls to three argument functions: ", nrow(calls_param3)))
 
-print(paste0("success rate for upto three argument functions for ", package, ": ", param123_success_rate))
-
-param_above_10 <- data[data[,3] > 10,]
+print(paste0("Success rate for up to three argument functions: ", calls_param123_success_rate))
 
 
 toc()
 total <- toc()
 
-meta_df <- data.frame(packge = package, num_fun = length(exported_functions), total_calls = nrow(data), success_all = success_rate, param123 = nrow(param123), success_param123 = param123_success_rate, time_total = (total$toc - total$tic), time_generating = (generating$toc - generating$tic))
+meta_df <- data.frame(packge = package, num_fun = length(exported_functions), budget = budget, tolerance = tolerance, total_calls = nrow(data), success_all = success_rate, num_fun_param123 = param1 + param2 + param3, calls_to_param123 = nrow(calls_param123), success_param123 = calls_param123_success_rate, time_total = (total$toc - total$tic), time_generating = (generating$toc - generating$tic))
 
 write.csv(meta_df, paste0(package, "_metadata.csv"), row.names=FALSE)
