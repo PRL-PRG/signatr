658f5363ad6ec9d5b22e907e0aee4227eaf7a99b
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-07-26 15:56:34 +0200

    pattern regex fixed

--- inst/combine_meta.R
+++ inst/combine_meta.R
@@ -5,14 +5,18 @@ options(error = function() { traceback(3); q(status=1) })
 args <- commandArgs(trailingOnly=TRUE)
 
 if (length(args) != 1) {
-  stop("Missing a path to running directory")
+  stop("Missing a path to the running directory")
 }
 
 OUTPUT_FILE <- "combined-meta.csv"
 
 run_dir <- args[1]
 
-files <- list.files(path = run_dir, pattern = "\\.csv$", recursive = TRUE, full.names = TRUE)
+if(!dir.exists(run_dir)) {
+  stop(paste0(run_dir, " does not exist"))
+}
+
+files <- list.files(path = run_dir, pattern = "*_metadata\\.csv$", recursive = TRUE, full.names = TRUE)
 
 meta <- lapply(files, function(f) read.csv(f))
 result <- do.call(rbind, meta)
