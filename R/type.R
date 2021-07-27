33dc8cf69dc3d619621b413d7137cf30db09eb7d
Yuan Cao <yuan.cao.email@gmail.com>
2021-07-27 16:16:07 -0400

    Abstract code to generate function types into its own function

--- R/type.R
+++ R/type.R
@@ -85,5 +85,55 @@ lub <- function(lot) {
 	list("any")
 }
 
+#' type_function: feedback loop result -> list of strings
+#' Find the types for a function based on the feedback loop result
 #'
+#' Examples:
+#' type_function(feedback_loop("stringr",
+#'							   "str_detect",
+#'							   budget = 343,
+#'							   tolerance = 1)) -> [[1]]
+#'												[1] "chr x chr x clx  ->  lgl"
+#'												....
 #'
+type_function <- function(run_result) {
+	rows = dim(run_result)[1]
+
+	argument_types = list()
+	return_types = list()
+	for(i in 1:rows) {
+		arguments = run_result[i, 4]
+		return_value = run_result[i, 5]
+
+		n = length(arguments$input)
+		arg_types = list()
+		for (i in 1:n) {
+			arg_types[[ length(arg_types) + 1 ]] = type(arguments$input[[i]])
+		}
+
+		argument_types[[ length(argument_types) + 1 ]] = arg_types
+
+		if (is.na(return_value[[1]])) {
+			return_types[[ length(return_types) + 1 ]] = NA
+		} else  {
+			return_types[[ length(return_types) + 1 ]] =  type(return_value[[1]])
+		}
+	}
+
+	types = list()
+	for (i in 1:rows) {
+		if (!is.na(return_types[[i]])) {
+			result = argument_types[[i]][[1]]
+			for (j in 2:length(argument_types[[i]])) {
+				result = paste(result, " x ", sep = "")
+				result = paste(result, argument_types[[i]][[j]], sep = "")
+			}
+
+			result = paste(result, " -> ", return_types[[i]])
+
+			types[[ length(types) + 1 ]] =  result
+		}
+	}
+
+	return(unique(types))
+}
