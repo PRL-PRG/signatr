e04378644e8c3968745f34ff135ac107ddf5c8e6
hyeyoung shin <hyeyoungshinw@gmail.com>
2021-08-06 12:43:48 +0200

    update merging script

--- inst/merge-dbs.R
+++ inst/merge-dbs.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
 start <- Sys.time()
-
 library(record)
 
 args <- commandArgs(trailingOnly=TRUE)
@@ -11,7 +10,6 @@ if (length(args) < 1) {
   q(status=1)
 }
 
-
 run_dir <- args[1]
 
 if (!dir.exists(run_dir)) {
@@ -19,26 +17,20 @@ if (!dir.exists(run_dir)) {
 }
 
 dirs <- list.dirs(path = run_dir, recursive = TRUE)
-
 dbs <- lapply(which(lapply(dirs, basename) == "db"), function(i) dirs[[i]])
-
 num_dbs <- length(dbs)
 
 cat(sprintf("Merging %s dbs\n\n", num_dbs))
 
 gbov <- paste0(run_dir, "/gbov")
-
 open_db(gbov, create = TRUE)
 
-for (db in dbs) {
-  merge_db(db)
-}
+lapply(dbs, function(db) merge_db(db))
 
 cat(sprintf("%s values are stored in gbov\n\n", size_db()))
 
 close_db()
 
 end <- Sys.time()
-
 round(end - start, digits = 2)
 
